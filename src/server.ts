282898365fce67ccd1c7fb766fc799c5e7e10ec6
Erkan Akdag <mafia001oh@gmail.com>
2026-02-19 11:53:03 +0100

    fix: jag instalerade sql och testade koden jag skrev. user.service.ts osv

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,32 @@
 import dotenv from "dotenv";
 import { createApp } from "./app";
+import { pool } from "./config/db";
+import { error } from "node:console";
 
 dotenv.config();
+
+pool.connect()
+.then (() => console.log("db connected suceesfully "))
+.catch((error) => console.error("Failed to connect to the database:", error));
+
+const startServer = async () => {
+  try {
+    // testar database connect
+    await pool.connect();
+    console.log("Connected to the database successfully.");
+    // Starta servern
+    const PORT = process.env.PORT ? Number(process.env.PORT) : 4000;
+    const app = createApp();
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to connect to the database:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 4000;
 
